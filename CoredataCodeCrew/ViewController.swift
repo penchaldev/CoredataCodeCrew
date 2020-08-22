97152ca91a9249ee854643c1102a7e22ad32fc06
Penchalreddy <penchal.vr@gmail.com>
2020-08-22 10:44:36 +0530

    Added TableView

--- CoredataCodeCrew/ViewController.swift
+++ CoredataCodeCrew/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var peopleTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Penchal"
+        cell.detailTextLabel?.text = "Age: 25"
+        
+        return cell
+    }
 }
 
